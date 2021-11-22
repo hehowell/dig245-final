672d0b734316b0f7377f424687a618ac71e64af8
hehowell <71607964+hehowell@users.noreply.github.com>
2021-11-22 04:05:28 -0500

    updated home page
    fixed button functionality and changed braille design

--- assets/js/create_map.js
+++ assets/js/create_map.js
@@ -46,10 +46,42 @@ function shuffle(array) {
 
 // Used like so
 
+window.onload = (event) => {
+	console.log('hey')
+	var arr = randomize(_args);
+	sessionStorage.setItem("places", arr);
+
+	var length = arr.length;
+
+	$("#clue").empty();
+
+	var randclue = Math.floor(Math.random() * clues.length);
+	if (clues[randclue]) {
+		console.log(clues[randclue]);
+		console.log(arr[length-1]);
 
 
+		$(".clue").append(
+			'<a class="clue" href= "assets/' +
+						arr[length-1] +
+						'_pages/' +
+						arr[length-1] +
+						'_' +
+						clues[randclue] +
+						'.html"><button><svg class="icon-arrow before"><use xlink:href="#arrow"></use>' +
+			'</svg><span class="label">Start your Adventure</span><svg class="icon-arrow after">' +
+			'<use xlink:href="#arrow"></use></svg></button><svg style="display: none;"><defs>' +
+			'<symbol id="arrow" viewBox="0 0 35 15"><title>Arrow</title>' +
+			'<path d="M27.172 5L25 2.828 27.828 0 34.9 7.071l-7.07 7.071L25 11.314 27.314 9H0V5h27.172z "/>' +
+			'</symbol></defs></svg></a>'
 
-$("button").on("click", function (e) {
+		);
+			}
+};
+
+
+function Rando () {
+	console.log('hey')
 	var arr = randomize(_args);
 	sessionStorage.setItem("places", arr);
 
@@ -87,4 +119,4 @@ $("button").on("click", function (e) {
 		// 	'<svg class="icon-arrow after"><use xlink:href="#arrow"></use></svg></button></div>'
 		// );
 	}
-});
+};
